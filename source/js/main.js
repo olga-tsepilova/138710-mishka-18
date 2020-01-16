98a2b3ed75e2d5dfb490d593d34368f2c26a8105
Ольга <o.tsepilova@gmail.com>
2020-01-17 00:44:33 +0600

    add картинки ретина

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,25 @@
-var link = document.querySelector(".login-link");
+/* mobile navigation */
+function classToggle() {
+  this.classList.toggle('nav__toggle--close');
+  document.querySelector('.nav__wrapper').classList.toggle('nav__wrapper--show');
+}
+document.querySelector('.nav__toggle').addEventListener('click', classToggle);
 
-var popup = document.querySelector(".modal-login");
-var close = popup.querySelector(".modal-close");
+
+/* modal window */
+var link = document.querySelector(".open-modal");
+
+var popup = document.querySelector(".modal");
+var close = popup.querySelector(".modal__close");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.toggle("modal-show");
+  popup.classList.toggle("modal--show");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.toggle("modal--show");
 });
 
 /*close.addEventListener("click", function (evt) {
@@ -16,8 +30,8 @@ link.addEventListener("click", function (evt) {
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (popup.classList.contains("modal-show")) {
-      popup.classList.remove("modal-show");
+    if (popup.classList.contains("modal--show")) {
+      popup.classList.remove("modal--show");
     }
   }
 });
