1e853e7fcf78c46ec6486b4c539aacf50202aabf
Ольга <o.tsepilova@gmail.com>
2020-01-27 01:22:14 +0600

    задача 9, проверки по комментариям, проверки на кроссбраузерность и прочие

--- source/js/main.js
+++ source/js/main.js
@@ -1,32 +1,11 @@
 "use strict";
 /* mobile navigation */
-// function classAddJs() {
-//   document.querySelector('.nav__wrapper').classList.add('nav__wrapper--close');
-// }
 function classToggle() {
   this.classList.toggle('nav__toggle--close');
   document.querySelector('.nav__wrapper').classList.toggle('nav__wrapper--close');
+  document.querySelector('.nav__wrapper').classList.toggle('nav__wrapper--show');
 }
 document.querySelector('.nav__toggle').addEventListener('click', classToggle);
-
-
-
-/*var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
-
-navMain.classList.remove('main-nav--nojs');
-
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-});*/
-
-
 
 /* modal window */
 var link = document.querySelector(".open-modal");
@@ -43,11 +22,6 @@ close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("modal--show");
 });
-
-/*close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal-show");
-});*/
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
